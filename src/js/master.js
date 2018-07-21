4b8e7b6a8cd461306072c3b161ed5723626dbd4c
root <ryanjmack21@gmail.com>
2018-07-21 07:39:44 -0400

    Update svg and scale functions

--- src/js/master.js
+++ src/js/master.js
@@ -2,27 +2,31 @@
 import { dataset } from './data.js';
 
 // constants for svg and graph dimensions
-const circleRadius = 7;
+const circleRadius = 6;
 const height = 500;
 const nonAxisPadding = 10;
 const width = 800;
 const xAxisPadding = 50;
 const yAxisPadding = 50;
 
-// set up the x and y scales
-const xScale = d3.scaleLinear()
-  .domain([d3.min(dataset, (cyclist) => cyclist.year), d3.max(dataset, (cyclist) => cyclist.year)])
-  .range([xAxisPadding, width - nonAxisPadding]);
-
-const yScale = d3.scaleLinear()
-  .domain([d3.max(dataset, (cyclist) => cyclist.seconds), d3.min(dataset, (cyclist) => cyclist.seconds)])
-  .range([height - yAxisPadding, nonAxisPadding]);
-
 // create the svg
 const svg = d3.select('main')
   .append('svg')
   .attr('height', height)
-  .attr('width', width);
+  .attr('width', width)
+  .attr('overflow', 'visible');
+
+
+// set up the x and y scales
+const xScale = d3.scaleLinear()
+  .domain([d3.min(dataset, (cyclist) => cyclist.year) - 1, d3.max(dataset, (cyclist) => cyclist.year)])
+  .range([xAxisPadding, width - nonAxisPadding]);
+
+const yScale = d3.scaleLinear()
+  // add a little buffer to the domain of inputs
+  .domain([d3.max(dataset, (cyclist) => cyclist.seconds) + 10, d3.min(dataset, (cyclist) => cyclist.seconds) - 10])
+  .range([height - yAxisPadding, nonAxisPadding]);
+
 
 // populate the scatterplot
 svg.selectAll('circle')
@@ -32,10 +36,11 @@ svg.selectAll('circle')
   .attr('cx', (cyclist) => xScale(cyclist.year))
   .attr('cy', (cyclist) => yScale(cyclist.seconds))
   .attr('r', circleRadius)
-  .attr('fill', (cyclist) => (cyclist.doping) ? "red" : "blue")
+  .attr('fill', (cyclist) => (cyclist.doping) ? "#e10d0d" : "#3fac03")
+  .attr('stroke', '#222')
   .append('title')
   .text(cyclist => {
-    let output = `${cyclist.name}: ${cyclist.nationality}\nTime: ${cyclist.time}`;
+    let output = `${cyclist.name}: ${cyclist.nationality}\nTime: ${cyclist.time}\nYear: ${cyclist.year}`;
 
     if (cyclist.doping) {
         output += `\n\n${cyclist.doping}\nSource: ${cyclist.url}`
